c89836d6c9eb32a99b0540debc29b1d4a2d0f78e
Rizky Maulana <rizkym71338@gmail.com>
2024-05-16 07:11:02 +0700

    mod(tailwind.conf): change fonsizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    container: {
-      center: true,
-    },
+    container: { center: true },
     extend: {
       colors: {
         'purple-1': '#7857FF',
@@ -23,93 +21,22 @@ const config: Config = {
         'blue-1': '#016FD5',
       },
       fontSize: {
-        'heading1-bold': [
-          '36px',
-          {
-            lineHeight: '100%',
-            fontWeight: '700',
-          },
-        ],
-        'heading2-bold': [
-          '30px',
-          {
-            lineHeight: '140%',
-            fontWeight: '700',
-          },
-        ],
-        'heading3-bold': [
-          '24px',
-          {
-            lineHeight: '140%',
-            fontWeight: '700',
-          },
-        ],
-        'heading4-bold': [
-          '20px',
-          {
-            lineHeight: '140%',
-            fontWeight: '700',
-          },
-        ],
-        'body-bold': [
-          '18px',
-          {
-            lineHeight: '140%',
-            fontWeight: '700',
-          },
-        ],
-        'body-normal': [
-          '18px',
-          {
-            lineHeight: '140%',
-            fontWeight: '500',
-          },
-        ],
-        'base-bold': [
-          '16px',
-          {
-            lineHeight: '140%',
-            fontWeight: '600',
-          },
-        ],
-        'small-bold': [
-          '14px',
-          {
-            lineHeight: '140%',
-            fontWeight: '600',
-          },
-        ],
-        'small-semibold': [
-          '14px',
-          {
-            lineHeight: '140%',
-            fontWeight: '500',
-          },
-        ],
-        'subtle-medium': [
-          '12px',
-          {
-            lineHeight: '16px',
-            fontWeight: '500',
-          },
-        ],
-        'tiny-medium': [
-          '10px',
-          {
-            lineHeight: '140%',
-            fontWeight: '500',
-          },
-        ],
-        'x-small-semibold': [
-          '7px',
-          {
-            lineHeight: '9.318px',
-            fontWeight: '600',
-          },
-        ],
+        'heading1-bold': ['36px', { lineHeight: '100%', fontWeight: '700' }],
+        'heading2-bold': ['30px', { lineHeight: '140%', fontWeight: '700' }],
+        'heading3-bold': ['24px', { lineHeight: '140%', fontWeight: '700' }],
+        'heading4-bold': ['20px', { lineHeight: '140%', fontWeight: '700' }],
+        'body-bold': ['18px', { lineHeight: '140%', fontWeight: '700' }],
+        'body-normal': ['18px', { lineHeight: '140%', fontWeight: '500' }],
+        'base-bold': ['16px', { lineHeight: '140%', fontWeight: '600' }],
+        'small-bold': ['14px', { lineHeight: '140%', fontWeight: '600' }],
+        'small-semibold': ['14px', { lineHeight: '140%', fontWeight: '500' }],
+        'subtle-medium': ['12px', { lineHeight: '16px', fontWeight: '500' }],
+        'tiny-medium': ['10px', { lineHeight: '140%', fontWeight: '500' }],
+        'x-small-semibold': ['7px', { lineHeight: '9px', fontWeight: '600' }],
       },
     },
   },
   plugins: [],
 }
+
 export default config
